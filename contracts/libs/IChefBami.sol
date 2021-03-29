d3e4e46461fc7a87d6c15c27f58ee422a6b78492
benjamin-tr <79494487+benjamin-tr@users.noreply.github.com>
2021-03-30 01:34:26 +0700

    [updated]: chefbami interface

--- contracts/libs/IChefBami.sol
+++ contracts/libs/IChefBami.sol
@@ -1,31 +1,44 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
+import "./IBEP20.sol";
+import "./ILocker.sol";
 
 interface IChefBami {
-  function poolLength() external view returns (uint256);
+    function poolLength() external view returns (uint256);
 
-  function addPool(
-    uint256 _allocPoint,
-    address _stakeToken,
-    bool _withUpdate
-  ) external;
+    function add(
+        uint256 _allocPoint,
+        IBEP20 _lpToken,
+        uint16 _depositFeeBP,
+        ILocker _locker,
+        bool _withUpdate
+    ) external;
 
-  function setPool(
-    uint256 _pid,
-    uint256 _allocPoint,
-    bool _withUpdate
-  ) external;
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        uint16 _depositFeeBP,
+        ILocker _locker,
+        bool _withUpdate
+    ) external;
 
-  function pendingBami(uint256 _pid, address _user) external view returns (uint256);
+    function pendingBami(uint256 _pid, address _user)
+        external
+        view
+        returns (uint256);
 
-  function updatePool(uint256 _pid) external;
+    function updatePool(uint256 _pid) external;
 
-  function deposit(address _for, uint256 _pid, uint256 _amount) external;
+    function deposit(
+        uint256 _pid,
+        uint256 _amount
+    ) external;
 
-  function withdraw(address _for, uint256 _pid, uint256 _amount) external;
+    function withdraw(
+        uint256 _pid,
+        uint256 _amount
+    ) external;
 
-  function withdrawAll(address _for, uint256 _pid) external;
-
-  function harvest(uint256 _pid) external;
+    function harvest(uint256 _pid) external;
 }
