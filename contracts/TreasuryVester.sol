ec51bdb2f212a9fff822f72d7264dd3c10d18c97
benjamin-tr <79494487+benjamin-tr@users.noreply.github.com>
2021-04-20 06:08:32 +0700

    [added] treasury vester

---
+++ contracts/TreasuryVester.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./libs/IBEP20.sol";
+
+contract TreasuryVester {
+    using SafeMath for uint;
+
+    address public bami;
+    address public recipient;
+
+    uint public vestingAmount;
+    uint public vestingBegin;
+    uint public vestingCliff;
+    uint public vestingEnd;
+
+    uint public lastUpdate;
+
+    constructor(
+        address bami_,
+        address recipient_,
+        uint vestingAmount_,
+        uint vestingBegin_,
+        uint vestingCliff_,
+        uint vestingEnd_
+    ) public {
+        require(vestingBegin_ >= block.timestamp, 'TreasuryVester::constructor: vesting begin too early');
+        require(vestingCliff_ >= vestingBegin_, 'TreasuryVester::constructor: cliff is too early');
+        require(vestingEnd_ > vestingCliff_, 'TreasuryVester::constructor: end is too early');
+
+        bami = bami_;
+        recipient = recipient_;
+
+        vestingAmount = vestingAmount_;
+        vestingBegin = vestingBegin_;
+        vestingCliff = vestingCliff_;
+        vestingEnd = vestingEnd_;
+
+        lastUpdate = vestingBegin;
+    }
+
+    function setRecipient(address recipient_) public {
+        require(msg.sender == recipient, 'TreasuryVester::setRecipient: unauthorized');
+        recipient = recipient_;
+    }
+
+    function claim() public {
+        require(block.timestamp >= vestingCliff, 'TreasuryVester::claim: not time yet');
+        uint amount;
+        if (block.timestamp >= vestingEnd) {
+            amount = IBEP20(bami).balanceOf(address(this));
+        } else {
+            amount = vestingAmount.mul(block.timestamp - lastUpdate).div(vestingEnd - vestingBegin);
+            lastUpdate = block.timestamp;
+        }
+        IBEP20(bami).transfer(recipient, amount);
+    }
+}
